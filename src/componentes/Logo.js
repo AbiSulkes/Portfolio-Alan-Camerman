737b3498bc6e998d50f359e72ab895d444e2d963
AbiSulkes <sulkesabi@gmail.com>
2023-11-21 14:43:21 -0300

    primeros cambios y estructuracion del sitio

---
+++ src/componentes/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            <img className='logo' src="https://abisulkes.alwaysdata.net/logoAlanCamerman.png" alt="logoAlanCamerman" />
+        </div>
+    );
+}
+
+export default Logo;
---
+++ src/componentes/Tipografia.js
@@ -0,0 +1,17 @@
+// FontComponent.js
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  font-family: 'Neue Haas Grotesk', sans-serif;
+  /* Agrega más estilos según sea necesario */
+`;
+
+const FontComponent = () => {
+  return (
+    <StyledContainer>
+      {/* Contenido de tu componente */}
+    </StyledContainer>
+  );
+};
+
+export default FontComponent;
